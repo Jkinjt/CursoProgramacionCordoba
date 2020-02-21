1444a27307288647d146f71b734b4487c30f0b8f
Jkinjt <l02jitoj@gmail.com>
2020-02-21 01:33:35 +0100

    buble y buble

---
+++ buble y blucle/buble y blucle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace buble_y_blucle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int casas = 0;
+                        int aldeanos = 0;
+            Console.WriteLine(casas);
+            do
+            {
+               casas = casas+1;
+               Console.WriteLine("casas: " + casas);
+                for (;  aldeanos < casas;)
+                {
+                    aldeanos = aldeanos + 1;
+                    Console.WriteLine("aldeanos: " + aldeanos);
+                    
+
+                    Console.WriteLine(aldeanos + "/" + casas);
+                }
+
+            } while (aldeanos <= 50);
+            Console.WriteLine(casas);
+        }
+    }
+}
